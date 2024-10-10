fd2895d9495d25c09f1504faec214e1e5bf51a5d
Jonathan Aguirre <de4519280@gmail.com>
2024-10-10 15:03:57 +0000

    Cambio para prueba de notificación en PR con etiqueta preview

--- src/pages/loginPage.js
+++ src/pages/loginPage.js
@@ -2,10 +2,6 @@ import { createClient} from '@supabase/supabase-js'
 import { Auth } from '@supabase/auth-ui-react'
 import { useNavigate } from 'react-router-dom';
 
-const supabase = createClient (
-    'https://nlwrmyrjpaaqtuimfduy.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5sd3JteXJqcGFhcXR1aW1mZHV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQxMTMyMzYsImV4cCI6MjAzOTY4OTIzNn0.K6OqJZCpAwJ02ii7Tf97cXdtoD2OC6HMZbaEPIayHI4'
-);
 
 function Login() {
     const navigate = useNavigate();
